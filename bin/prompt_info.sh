1517c95b9193c266f367408ba559e47aeec78191
Flávio Caetano <caetano@work.co>
2024-07-23 13:59:39 -0400

    feat: add proxyman scripts

---
+++ bin/prompt_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ $PROXYMAN_INJECTION_ACTIVE ]]; then
+  echo "🚀 "
+else
+  echo ""
+fi
---
+++ bin/proxyman_deactivate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export CARGO_HTTP_CAINFO=
+export CGI_HTTP_PROXY=
+export CURL_CA_BUNDLE=
+export GIT_SSL_CAINFO=
+export GLOBAL_AGENT_HTTP_PROXY=
+export HTTPS_PROXY=
+export HTTP_PROXY=
+export NODE_EXTRA_CA_CERTS=
+export NODE_OPTIONS=
+export NODE_TLS_REJECT_UNAUTHORIZED=
+export PERL_LWP_SSL_CA_FILE=
+export PROXYMAN_INJECTION_ACTIVE=
+export PROXYMAN_INJECTION_OVERRIDE_PATH=
+export PYTHONPATH=
+export REQUESTS_CA_BUNDLE=
+export RUBYLIB=
+export SPACESHIP_PROXY=
+export SPACESHIP_PROXY_SSL_VERIFY_NONE=
+export SSL_CERT_FILE=
+export http_proxy=
+export https_proxy=
+export npm_config_https_proxy=
+export npm_config_proxy=
+export npm_config_scripts_prepend_node_path=
+alias deactivate=
+
+echo "----------------------------------------------"
+echo "------PROXYMAN AUTOMATIC SETUP SCRIPTS--------"
+echo "----------------------------------------------"
+echo "🛑 Proxyman deactivated"
